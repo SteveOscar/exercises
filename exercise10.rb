55c6d8a95c725285970413df65048e758436afcd
Steven Olson <steveoscaro@gmail.com>
2015-09-16 12:06:25 -0600

    exercise 10 tealeaf

---
+++ exercise10.rb
@@ -0,0 +1,9 @@
+#yes, hash values can be arrays. Example:
+
+hash = {a: [1, 2, 3], b: [4, 5, 6]}
+
+#yes, you can also have an array of hashes. Example:
+
+array = [{a: 1, b: 2}, {c: 3, d: 4}]
+
+p array
